b5fe94f481a5b74406646a54869d68c0482520c0
Raudah Sapna Maharani <91400731+rsapnamhrn@users.noreply.github.com>
2025-02-28 21:28:24 +0700

    update/index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 01, 2025',
+  birthdate: 'Mar 01, 2025',
   name: '❤️❤️ My Alfian ❤️❤️',
   message: 'I Love You So Much!'
 };
